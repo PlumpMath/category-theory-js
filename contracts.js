3f058930cead4acefbe6ad4864cbd357cdf92652
Victor Igor <victor.0w3@hotmail.com>
2017-05-06 19:36:10 -0300

    guarded functions

--- contracts.js
+++ contracts.js
@@ -1,6 +1,7 @@
 //The contracts helps us check the type of things before we work on them.
 
-const str = (s) => {
+//Contract -> Objects in Category Theory
+const str = s => {
 	if(typeof s  !== 'string'){
 		throw new Error('Expected a string!');
 	}else{
@@ -8,7 +9,8 @@ const str = (s) => {
 	}
 };
 
-const repeat = (s) => {
+//Guarded functions -> Morphism
+const repeat = s => {
 	s = str(s);
 	return s + s;
 };
@@ -16,3 +18,31 @@ const repeat = (s) => {
 //example:
 // console.log(repeat(2)); //-> Error: Expected string!
 // console.log(repeat("2")); //-> 22
+
+
+const any = x => x;
+
+
+const typeOf = type => {
+	type = str(type);
+	return (s) => {
+		if(typeof s  !== type){
+			throw new Error(`Expected a ${type}!`);
+		}else{
+			return s;
+		}
+	};
+};
+
+//others primitive types
+const bool = typeOf('boolean');
+const obj = typeOf('object');
+const num = typeOf('number');
+const undef = typeOf("undefined");
+const fun = typeOf("function");
+
+//Number -> Number  :: Guarded function
+const inc = x => {
+	x = num(x);
+	return num(x + 1);
+};
